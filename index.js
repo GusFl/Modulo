5a2e1c01259f070e7172be54a3b8e0ff75c60f2a
GusFl <gusflores0127@hotmail.com>
2023-08-28 08:27:18 -0500

    Agregando index

---
+++ index.js
@@ -0,0 +1,13 @@
+let frases=[
+        "FRASE 1",
+        "FRASE 2",
+        "FRASE 3"
+]
+
+function obtieneFrase(indice) {
+    return frases(indice)  
+}
+
+console.log(module);
+
+module.exports.obtieneFrase=obtieneFrase;
